1a2ba0ace511fc9c46ebf636cc74f314a413568a
RaniaTmar <131198083+RaniaTmar@users.noreply.github.com>
2026-05-11 20:52:16 +0100

    Update API URLs to use window.location.origin

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl:    `${window.location.protocol}//${window.location.hostname}:31844`,
-  geoApiUrl: `${window.location.protocol}//${window.location.hostname}:31844`
+  apiUrl: window.location.origin,
+  geoApiUrl: window.location.origin
 };
